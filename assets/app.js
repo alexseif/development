8cc8583770adc794d9b699d07a11c2fde56e4fb3
Alex Seif <alex.seif@gmail.com>
2022-04-29 13:01:10 +0200

    Calender view & layout

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,6 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 import 'jquery'
@@ -14,6 +12,7 @@ import 'bootstrap'
 import 'datatables.net-bs5'
 import 'datatables.net-rowreorder';
 import 'datatables.net-rowreorder-bs5';
+import './styles/app.scss';
 
 const $ = require('jquery');
 Window.prototype.$ = $;
---
+++ assets/calendar.js
@@ -0,0 +1,4 @@
+/**
+ * The following content was designed & implemented under AlexSeif.com
+ **/
+import './styles/calendar.scss';
