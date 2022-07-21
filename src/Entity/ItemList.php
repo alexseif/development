265a3170cdf2a92122ad5ba43cd348f801d5434e
Alex Seif <alex.seif@gmail.com>
2022-07-21 19:36:06 +0200

    adding timestampable to itemlist and using namedentity

--- src/Entity/ItemList.php
+++ src/Entity/ItemList.php
@@ -2,21 +2,23 @@
 
 namespace App\Entity;
 
+use App\Entity\Traits\NamedEntity;
 use App\Repository\ItemListRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: ItemListRepository::class)]
 class ItemList
 {
+    use NamedEntity;
+    use TimestampableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $name;
 
     #[ORM\OneToMany(mappedBy: 'itemList', targetEntity: Item::class)]
     private $items;
@@ -29,18 +31,6 @@ class ItemList
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     /**
